455b099150b19343b71b32a47a7cbe318791b39f
ruudslagers <r.slagers@student.fontys.nl>
2017-06-21 17:53:43 +0200

    Framework - werkend

---
+++ js/aanpassen.js
@@ -0,0 +1,11 @@
+var num = 10,       //Verander dit nummer voor meer bolletjes tijdens het laden
+    ang = 360/num,  //Verander dit voor hoeveel graden je de bolletjes wilt laten draaien
+    rad = num*5,    //Verander dit voor de spacing tussen de bolletjes
+    run = 2;
+
+function aanpassen() {
+    window.num;
+    window.ang;
+    window.rad;
+    window.run;
+}
